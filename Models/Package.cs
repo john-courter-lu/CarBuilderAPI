d426a84901505efb724d13f41aa048416fb6df6a
John Lu <johnlu2006@gmail.com>
2023-08-25 13:41:02 -0500

    initialize classes under Models folder

---
+++ Models/Package.cs
@@ -0,0 +1,8 @@
+namespace CarBuilderAPI.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/Wheels.cs
@@ -0,0 +1,9 @@
+namespace CarBuilderAPI.Models
+{
+    public class Wheels
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Style { get; set; }
+    }
+}
