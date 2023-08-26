65ef1333f7801f86ae357df5a3f289c8c71e33a3
John Lu <johnlu2006@gmail.com>
2023-08-25 21:05:51 -0500

    add 4 more properties to class Order to store data related to ...Id

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,5 +8,9 @@ public class Order
     public int TechnologyId { get; set; }
     public int PaintId { get; set; }
     public int InteriorId { get; set; }
+    public Wheels Wheels { get; set; } // Updated property name
+    public Technology Technology { get; set; }
+    public PaintColor Paint { get; set; }
+    public Interior Interior { get; set; }
 }
 
